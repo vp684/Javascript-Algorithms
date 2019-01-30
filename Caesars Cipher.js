16391dd1afe5c8d6b708778b2ed90d1d347f53aa
Viraj <viraj84@gmail.com>
2019-01-30 13:08:57 -0500

    Create Caesars Cipher.js

---
+++ Caesars Cipher.js
@@ -0,0 +1,19 @@
+function rot13(str) { // LBH QVQ VG!
+  const rotated = 'NOPQRSTUVWXYZABCDEFGHIJKLM';
+  const original = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var decoded = ''
+  let rgx = /[a-zA-Z]/;  
+
+  for(let i = 0; i < str.length; i++){
+     
+     if(str[i].search(rgx) === -1){ // non alphabet so pass
+        decoded += str[i];
+     }
+     else{//must be a  letter
+        const rotindex = rotated.search(str[i])
+        decoded += original[rotindex];
+     }
+
+  }  
+  return decoded;
+}
